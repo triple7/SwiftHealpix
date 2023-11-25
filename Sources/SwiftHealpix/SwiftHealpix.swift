6067e9d7851bcfb3bb7bb2436af1d5d794516bab
Yuma Antoine Decaux <yuma@astreos.space>
2023-11-25 23:01:56 +1000

    HP typealias for Healpix

--- Sources/SwiftHealpix/SwiftHealpix.swift
+++ Sources/SwiftHealpix/SwiftHealpix.swift
@@ -9,6 +9,8 @@ import Foundation
 import simd
 import BigInt
 
+public typealias HP = Healpix
+
 public final class Healpix {
     private let NSIDELIST:[Int] = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216]
     private let NS_MAX = 16777216
